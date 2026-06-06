5f8487177bed26ce21a0028964b6040b5d7a2767
the179050-beep <the179050@gmail.com>
2026-06-06 05:43:49 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { FullPageLoader } from "@/components/loader";
 import { ChatPanel } from "@/components/chat-panel";
